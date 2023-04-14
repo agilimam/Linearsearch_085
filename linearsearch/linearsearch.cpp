4cf504d536b3c92a7513783ee4ac59a2c6897cd3
agilimam <agilimamalhafizi04@gmail.com>
2023-04-14 14:50:42 +0700

    membuat maximun dan minimun element

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -8,3 +8,12 @@ int i; // Index of array element
 
 void input()
 {
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimun 1 and maximun 20 elements.\n\n";
+	}
